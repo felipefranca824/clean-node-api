6fed9c759681c8b16b87decea367687ac09226e3
Felipe França <felipefranca824@gmail.com>
2023-06-21 14:05:00 -0300

    create middlewares

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,10 @@
 import { type Express } from 'express'
 import { bodyParser } from '../middlewares/body_parser'
+import { cors } from '../middlewares/cors'
+import { contentType } from '../middlewares/content-type'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
+  app.use(contentType)
 }
