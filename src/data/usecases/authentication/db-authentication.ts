8914446c15e6cff2b30eb2aa6d6f6b229d4923ba
Felipe França <felipefranca824@gmail.com>
2023-07-18 15:41:45 -0300

    refactor imports

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,8 +1,11 @@
-import { type Authentication, type AuthenticationModel } from '../../../domain/usecases/authentication'
-import { type HashComparer } from '../../protocols/criptography/hash-comparer'
-import { type TokenGenerator } from '../../protocols/criptography/token-generator'
-import { type LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
-import { type UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
+import {
+  type UpdateAccessTokenRepository,
+  type LoadAccountByEmailRepository,
+  type TokenGenerator,
+  type HashComparer,
+  type AuthenticationModel,
+  type Authentication
+} from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
   constructor (
