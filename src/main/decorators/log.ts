81d368d9e5d578f3f830435dbfb9ea98848dbee3
Felipe França <felipefranca824@gmail.com>
2023-07-17 21:01:54 -0300

    add error in mongo db

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -6,7 +6,7 @@ export class LogControllerDecorator implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
     if (httpResponse.statusCode === 500) {
-      await this.logErrorRepositoryStub.log(httpResponse.body.stack)
+      await this.logErrorRepositoryStub.logError(httpResponse.body.stack)
     }
     return httpResponse
   }
