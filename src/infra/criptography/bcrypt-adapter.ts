d10c11a56f8e7c788bb8726318d9e4630ac7fa83
Felipe França <felipefranca824@gmail.com>
2023-07-18 16:54:55 -0300

    rename encrypter to hasher

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -1,8 +1,8 @@
-import { type Encrypter } from '../../data/protocols/criptography/encrypter'
+import { type Hasher } from '../../data/protocols/criptography/hasher'
 import bcrypt from 'bcrypt'
-export class BcryptAdapter implements Encrypter {
+export class BcryptAdapter implements Hasher {
   constructor (private readonly salt: number) {}
-  async encrypt (value: string): Promise<string> {
+  async hash (value: string): Promise<string> {
     const hash = await bcrypt.hash(value, this.salt)
     return hash
   }
