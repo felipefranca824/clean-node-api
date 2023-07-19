df165d40490154621a56abf1d1590b5a3a1452b2
Felipe França <felipefranca824@gmail.com>
2023-07-18 23:38:39 -0300

    load account by email implementation

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,12 +1,27 @@
 import { type AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
+import { type LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
 import { type AccountModel } from '../../../../domain/models/account'
 import { type AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     return MongoHelper.map(result, accountData)
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel | null> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ email })
+    if (account) {
+      return {
+        id: account._id.toString(),
+        name: account.name,
+        email: account.email,
+        password: account.password
+      }
+    }
+    return null
   }
 }
