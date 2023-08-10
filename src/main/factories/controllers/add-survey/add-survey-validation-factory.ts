e7c1fecae18c5c30588724253c6f2727d3c763a8
Felipe França <felipefranca824@gmail.com>
2023-08-10 11:00:48 -0300

    add survey composition

---
+++ src/main/factories/controllers/add-survey/add-survey-validation-factory.ts
@@ -0,0 +1,11 @@
+import { type Validation } from '../../../../presentation/controllers/auth/login/login-controller-protocols'
+import { ValidationComposite, RequiredFieldValidation } from '../../../../validation/validators'
+
+export const makeAddSurveyValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of ['question', 'answers']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+
+  return new ValidationComposite(validations)
+}
