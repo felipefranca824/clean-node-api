b1c418be7522dc13b651bece06f4a57a8830aa73
Felipe França <felipefranca824@gmail.com>
2023-10-23 14:25:56 -0300

    add no cache middleware

--- src/main/config/config-swagger.ts
+++ src/main/config/config-swagger.ts
@@ -1,7 +1,8 @@
 import { type Express } from 'express'
+import { noCache } from '../middlewares/no-cache'
 import { serve, setup } from 'swagger-ui-express'
 import swaggerConfig from '@/main/docs'
 
 export default (app: Express): void => {
-  app.use('/api-docs', serve, setup(swaggerConfig))
+  app.use('/api-docs', noCache, serve, setup(swaggerConfig))
 }
---
+++ src/main/middlewares/no-cache.ts
@@ -0,0 +1,9 @@
+import { type NextFunction, type Request, type Response } from 'express'
+
+export const noCache = (req: Request, res: Response, next: NextFunction): void => {
+  res.set('cache-control', 'no-cache, no-store, must-revalidate, proxy-revalidate')
+  res.set('pragma', 'no-cache')
+  res.set('expires', '0')
+  res.set('surrogate-control', 'no-store')
+  next()
+}
